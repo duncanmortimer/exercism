6a73a0bd97544832d994d0ba71bd10e12a18dd9c
Duncan Mortimer <dmorti@gmail.com>
2019-08-24 17:11:10 +0100

    python: Add isogram solution

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,5 @@
-def is_isogram(string):
-    pass
+import re
+
+def is_isogram(string: str) -> bool:
+    relevant_characters = re.sub(r"[^a-z]", "", string.lower())
+    return len(set(relevant_characters)) == len(relevant_characters)
