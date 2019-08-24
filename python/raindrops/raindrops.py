a1bc17d819fe7e154756d8e0d1a4356501b85670
Duncan Mortimer <dmorti@gmail.com>
2019-08-24 16:55:35 +0100

    python: Add solution to raindrops

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,15 @@
-def convert(number):
-    pass
+from typing import Tuple
+
+FACTOR_TO_SOUND = (
+    (3, "Pling"),
+    (5, "Plang"),
+    (7, "Plong"),
+)
+
+def convert(number: int):
+    sounds: Tuple[str] = [
+        sound
+        for factor, sound in FACTOR_TO_SOUND
+        if number % factor == 0
+    ]
+    return "".join(sounds) if sounds else str(number)
