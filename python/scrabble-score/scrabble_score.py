00e206c73fd19042915484e0591c924b5b277a90
Duncan Mortimer <dmorti@gmail.com>
2019-08-25 03:25:08 +0100

    python: Add scrabble-score solution

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,21 @@
+from collections import Counter
+
+LETTER_SCORE = {
+    letter: score
+    for letters, score in [
+        ("A, E, I, O, U, L, N, R, S, T", 1),
+        ("D, G", 2),
+        ("B, C, M, P", 3),
+        ("F, H, V, W, Y", 4),
+        ("K", 5),
+        ("J, X", 8),
+        ("Q, Z", 10),
+    ]
+    for letter in letters.split(", ")
+}
+
 def score(word):
-    pass
+    return sum(
+        LETTER_SCORE[letter] * count
+        for letter, count in Counter(word.upper()).items()
+    )
