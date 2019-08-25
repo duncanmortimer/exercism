1d57dd46db2cf64673127a61f834030a93fc1e83
Duncan Mortimer <dmorti@gmail.com>
2019-08-25 03:11:39 +0100

    python: Add solution to word-count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,13 @@
+from collections import Counter
+from functools import partial
+import re
+
+
+STRIP_LEFT_QUOTE = partial(re.sub, r"^'", "")
+STRIP_RIGHT_QUOTE = partial(re.sub, r"'$", "")
+UNQUOTE = lambda word: STRIP_LEFT_QUOTE(STRIP_RIGHT_QUOTE(word))
+
+
 def count_words(sentence):
-    pass
+    words = re.findall(r"[a-z0-9']+", sentence.lower())
+    return Counter(map(UNQUOTE, words))
