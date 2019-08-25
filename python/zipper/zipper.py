c218f0bb9e477901880724ced3d66cc0169b0c43
Duncan Mortimer <dmorti@gmail.com>
2019-08-25 10:04:08 +0100

    python: Add zipper solution

--- python/zipper/zipper.py
+++ python/zipper/zipper.py
@@ -1,28 +1,81 @@
-class Zipper(object):
+from functools import reduce
+
+
+LEFT, VALUE, RIGHT = "left", "value", "right"
+
+
+class Zipper:
+    """
+    A Binary-tree-with-zipper for python.
+    """
+
+    @property
+    def _left_branch(self):
+        return self._location[LEFT]
+
+    @property
+    def _right_branch(self):
+        return self._location[RIGHT]
+
+    @property
+    def _value(self):
+        return self._location[VALUE]
+
     @staticmethod
     def from_tree(tree):
-        pass
+        return Zipper(path=[], location=tree)
+
+    def __init__(self, path, location):
+        self._path = path
+        self._location = location
 
     def value(self):
-        pass
+        return self._value
 
-    def set_value(self):
-        pass
+    def set_value(self, new_value):
+        return Zipper(
+            path=self._path,
+            location={LEFT: self._left_branch, VALUE: new_value, RIGHT: self._right_branch}
+        )
 
     def left(self):
-        pass
+        if self._left_branch is None:
+            return None
 
-    def set_left(self):
-        pass
+        return Zipper(
+            path=[lambda x: {LEFT: x, VALUE: self._value, RIGHT: self._right_branch}] + self._path,
+            location=self._left_branch
+        )
+
+    def set_left(self, new_left):
+        return Zipper(
+            path=self._path,
+            location={LEFT: new_left, VALUE: self._value, RIGHT: self._right_branch}
+        )
 
     def right(self):
-        pass
+        if self._right_branch is None:
+            return None
 
-    def set_right(self):
-        pass
+        return Zipper(
+            path=[lambda x: {LEFT: self._left_branch, VALUE: self._value, RIGHT: x}] + self._path,
+            location=self._right_branch
+        )
+
+    def set_right(self, new_right):
+        return Zipper(
+            path=self._path,
+            location={LEFT: self._left_branch, VALUE: self._value, RIGHT: new_right}
+        )
 
     def up(self):
-        pass
+        if not self._path[0]:
+            return None
+
+        return Zipper(
+            path=self._path[1:],
+            location=self._path[0](self._location)
+        )
 
     def to_tree(self):
-        pass
+        return reduce(lambda v, f: f(v), self._path, self._location)
